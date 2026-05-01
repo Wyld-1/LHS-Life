61513e4925c94815be5a28adcb9f860249b9059f
Wyld-1 <liam.lefohn+github@gmail.com>
2026-05-01 12:42:19 -0700

    Initial Commit

---
+++ LaSalle Schedule/LaSalle_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaSalle_ScheduleApp.swift
+//  LaSalle Schedule
+//
+//  Created by Liam Lefohn on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct LaSalle_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaSalle ScheduleTests/LaSalle_ScheduleTests.swift
@@ -0,0 +1,19 @@
+//
+//  LaSalle_ScheduleTests.swift
+//  LaSalle ScheduleTests
+//
+//  Created by Liam Lefohn on 5/1/26.
+//
+
+import Testing
+@testable import LaSalle_Schedule
+
+struct LaSalle_ScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
